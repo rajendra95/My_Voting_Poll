fa18e72047c1fa351a1db0cd0625146047f5ad26
rajendra95 <36725053+rajendra95@users.noreply.github.com>
2019-04-24 11:19:47 +0530

    Update README.md

--- handlers.go
+++ handlers.go
@@ -1,74 +1,142 @@
 package main
 
 import (
-	"crypto/bcrypt"
 	"database/sql"
-	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-// struct for getting the user credentials - username password
-
-type Credentials struct {
-	Password string `json:"password",db:"password"`
-	Username string `json:"username",db:"username"`
-}
-
-// we will write handlers :
+var err error
 
 func Signup(w http.ResponseWriter, r *http.Request) {
-	// parse and decode the request body into the new credentials instance
-	creds := Credentials{}
-	err := json.NewDecoder(r.Body).Decode(creds)
-	if err != nil {
-		// If something went wrong write 400 bad request in the response.
-		w.WriteHeader(http.StatusBadRequest)
-	}
-	return
-	// if OK -> encrypt the password using bcrypt algorithm
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), 8)
 
-	// now we have to insert this encrypted value into a database
-	if _, err = db.Query("insert the values ($1,$2)", creds.Username, string(hashedPassword)); err != nil {
-		// if we faced an issue in inserting the data into the database then we need to send 500 error
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-	return
-	// if everything went alright then we data will get stored inside the database and default status code 200 will be return.
-}
-
-func Signin(w http.ResponseWriter, r *http.Request) {
-	// parse and decode the request body into creds instance
-	creds := Credentials{}
-	err := json.NewDecoder(r.Body).Decode(creds)
-	if err != nil {
-		// If something went wrong write 400 bad request in the response.
-		w.WriteHeader(http.StatusBadRequest)
-	}
-	// Get the existing entry present in the database for the given username
-	result := db.QueryRow("select password from users based on username ", creds.Username)
-
-	// We create another instance of `Credentials` to store the credentials we get from the database
-	storedCreds := Credentials{}
-	err = result.Scan(&storedCreds.Password)
-	if err != nil {
-		// There are two possibilites now - (Either The username will not be there in the database OR Internal server Error)
-
-		// If an entry with the username does not exist, send an "Unauthorized"(401) status
-		if err == sql.ErrNoRows {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		// If the error is of any other type, send a 500 status
-		w.WriteHeader(http.StatusInternalServerError)
+	if r.Method != "POST" {
+		http.ServeFile(w, r, "signup.html")
 		return
 	}
-	// compare the stored and received credentials
-	if err = bcrypt.CompareHashAndPassword([]byte(storedCreds.Password), []byte(creds.Password)); err != nil {
-		// If two passwords does not match then return 401 unauthorized error
-		w.WriteHeader(http.StatusUnauthorized)
-	}
-	// If we reach this point, that means the users password was correct, and that they are authorized
-	// The default 200 status is sent
+	username := r.FormValue("username")
+	password := r.FormValue("password")
 
+	var user string
+
+	rows, err := db.Query("SELECT username FROM users WHERE username=?", username)
+	if err != nil {
+		panic(err.Error())
+	}
+	count := 0
+	for rows.Next() {
+		count++
+	}
+	if count > 0 {
+		w.Write([]byte("USER already existed"))
+		return
+	}
+	err = db.QueryRow("SELECT username FROM users WHERE username=?", username).Scan(&user)
+	switch {
+	case err == sql.ErrNoRows:
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			http.Error(w, "Unable to crate accout right now (ONE)", http.StatusInternalServerError)
+			return
+		}
+		_, err = db.Exec("INSERT INTO users(username,password) VALUES(?,?)", username, hashedPassword)
+		if err != nil {
+			http.Error(w, "Unable to create your account (TWO)", http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte("User Accout Created"))
+
+	case err != nil:
+		http.Error(w, "Unable to create your account (THREE)", http.StatusInternalServerError)
+		return
+	default:
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+	}
+
+	http.Redirect(w, r, "/", http.StatusOK)
+}
+func Login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.ServeFile(w, r, "login.html")
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	var dbusername string
+	var dbpassword string
+
+	err = db.QueryRow("SELECT username, password FROM users WHERE username=?", username).Scan(&dbusername, &dbpassword)
+	if err != nil {
+		w.Write([]byte("User Does Not Exist"))
+		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+		return
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(dbpassword), []byte(password))
+	if err != nil {
+		w.Write([]byte("Credentials Did not Match!"))
+		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+		return
+	}
+
+	http.Redirect(w, r, "/register", 301)
+}
+
+func Homepage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+func Register(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		http.ServeFile(w, r, "register.html")
+	}
+
+	http.Redirect(w, r, "/stored", 301)
+
+}
+
+func Storedb(w http.ResponseWriter, r *http.Request) {
+
+	var VoterID string
+	var LastName string
+	var FirstName string
+	var Age string
+	var Sex string
+	var State string
+	var City string
+
+	switch r.Method {
+
+	case "GET":
+
+		http.ServeFile(w, r, "db.html")
+	case "POST":
+
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+
+		VoterID = r.FormValue("VoterID")
+		LastName = r.FormValue("LastName")
+		FirstName = r.FormValue("FirstName")
+		State = r.FormValue("State")
+		City = r.FormValue("City")
+		Age = r.FormValue("Age")
+		Sex = r.FormValue("Sex")
+
+	default:
+		fmt.Fprintf(w, "Service only Supports GET and POST")
+	}
+
+	_, err = db.Exec("INSERT INTO Voters(VoterID,LastName,FirstName,Age,Sex,State,City)VALUES(?,?,?,?,?,?,?)", VoterID, LastName, FirstName, Age, Sex, State, City)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	w.Write([]byte(" Registered Successully!"))
 }
