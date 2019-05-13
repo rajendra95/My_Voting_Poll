14879c08600b36695923c574853426b8442fe5af
Rajendra Gosavi <36725053+rajendragosavi@users.noreply.github.com>
2019-05-13 17:33:03 +0530

    Update main.go

--- main.go
+++ main.go
@@ -22,6 +22,8 @@ func main() {
 	http.HandleFunc("/", Homepage)
 	http.HandleFunc("/register", Register)
 	http.HandleFunc("/stored", Storedb)
+	http.HandleFunc("/vote", Vote)
+	http.HandleFunc("/terms&conditions", TermsandConditions)
 	// initialize database
 	initDB()
 	log.Fatal(http.ListenAndServe(":8085", nil))
